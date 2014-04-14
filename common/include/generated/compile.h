b9f44fe92bceb5431207eec0bdc4d1e1e3d19475
bieltv3 <bieltv.3@gmail.com>
2014-04-14 09:05:54 +0200

    Fix sources on new path: WORKING_DIRECTORY/kernel/samsung/bcm21553-common/common

--- common/include/generated/compile.h
+++
@@ -1,9 +0,0 @@
-/* This file is auto generated, version 2 */
-/*  PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#2 PREEMPT Fri Feb 7 17:51:19 CET 2014"
-#define LINUX_COMPILE_TIME "17:51:19"
-#define LINUX_COMPILE_BY "bieltv3"
-#define LINUX_COMPILE_HOST "bieltv3"
-#define LINUX_COMPILE_DOMAIN "(none)"
-#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
